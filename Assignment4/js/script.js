c469c5a74e307be8ead2cc60e6d495a111826d0a
Rohan arya <47821908+rohanrvit@users.noreply.github.com>
2021-08-09 17:30:38 +0530

    Update script.js

--- Assignment4/js/script.js
+++ Assignment4/js/script.js
@@ -4,12 +4,10 @@ function CreateAccountViewModel() {
     self.firstName = ko.observable("").extend({
        required: true,
        minLength: 2,
-       
     });
     self.lastName = ko.observable("").extend({
         required: true,
         minLength: 2,
-        
      });
     self.emailAddress = ko.observable("").extend({
        required: true,
@@ -17,55 +15,25 @@ function CreateAccountViewModel() {
     });
     this.course = ko.observable([]).extend({required:true}),
     this.gender = ko.observable("").extend({required:true}),
-    // var x = {
-
-    //     courses: ko.observable(false),
-
-    //     a: ko.observableArray(["JAVA", "PYTHON","C++"])
-
-    // };
-
-    // x.a.push("msg");
-    
     self.hasBeenSubmitted = ko.observable(false);
-    
-    
-   
-    
-    self.handleSubmit = function() {
-       
-       //Check for errors      
+    self.handleSubmit = function() {   
        var errors = ko.validation.group(self);
        if (errors().length > 0) {
           errors.showAllMessages();
           return;
        }
- 
        self.hasBeenSubmitted(true);
-       
-       //Form is valid!
        console.log('submit the form!')
-       //Api call would go here...
-       //
-       //
-      
        const value1= {
             firstName: self.firstName(),
            lastName: this.lastName(),
             emailAddress: self.emailAddress(),
             gender: self.gender(),
-            courses:self.course()
-          
-  
+            courses:self.course()     
          }
-       
-   
          const myJSON = JSON.stringify(value1);
          console.log(myJSON)
     }
-   
-   
  };
- 
  const knockoutApp = document.querySelector("#knockout-app");
  ko.applyBindings(new CreateAccountViewModel(), knockoutApp);
